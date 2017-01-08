8c9fef466bbfe289c9fcd955d9f1cc436b3939fc
veean <nersesowm@gmail.com>
2017-01-09 01:20:08 +0400

    optimize the find_possible_duplicates method

--- duplicates.py
+++ duplicates.py
@@ -2,13 +2,13 @@ import os
 import sys
 
 
-def find_duplicates(path):
+def find_possible_duplicates(path):
     size_and_path_table = {}
     for dirs, _, files in os.walk(path):
         for file in files:
             path_to_file = os.path.join(dirs, file)
             file_size = os.path.getsize(path_to_file)
-            if file_size in size_and_path_table:
+            if size_and_path_table.get(file_size, False):
                 size_and_path_table[file_size].append(path_to_file)
             else:
                 size_and_path_table[file_size] = [path_to_file]
@@ -19,15 +19,16 @@ def has_duplicates(filename_entries):
     return len(filename_entries) > 1
 
 
-def are_files_duplicates(path_to_analyze):
-    files_table = find_duplicates(path_to_analyze)
-    results = [x for x in files_table.values() if has_duplicates(x)]
-    if results:
-        for result in results:
-            for result_part in result:
-                print(result_part)
-    else:
-        print('None of duplicate files')
+def get_files_duplicates(path_to_analyze):
+    files_table = find_possible_duplicates(path_to_analyze)
+    return [x for x in files_table.values() if has_duplicates(x)]
+
+
+def print_files_duplicates(list_of_duplicates):
+    print('A list of duplicate files : ')
+    for duplicate in list_of_duplicates:
+        for duplicate_entry in duplicate:
+            print(duplicate_entry)
 
 
 if __name__ == '__main__':
@@ -38,8 +39,6 @@ if __name__ == '__main__':
             if not os.path.exists(start_path):
                 print("Invalid directory!")
             else:
-                print('Duplicate files list :')
-                are_files_duplicates(start_path)
-
+                print_files_duplicates(get_files_duplicates(start_path))
     except IndexError as e:
         print("Choose directory to search ...")
